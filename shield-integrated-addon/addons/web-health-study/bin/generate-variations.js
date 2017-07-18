e0e5434ae89d9ff82e511216026557c06a569b5b
Kamyar Ardekani <kamyar@mozilla.com>
2017-07-18 11:28:08 +0200

    atsi added

--- shield-integrated-addon/addons/web-health-study/bin/generate-variations.js
+++ shield-integrated-addon/addons/web-health-study/bin/generate-variations.js
@@ -2,11 +2,12 @@ const MESSAGES = {
   c: '',
   vi: 'Net neutrality is one of the biggest First Amendment issues of the Internet. Without it, voices could be censored.',
   sice: 'Support Net Neutrality like {domain}, so that the Internet remains healthy and free',
-  simo: 'Three quarters of Americans (76%) support net neutrality, including  the vast majority of Democrats and Republicans.'
+  simo: 'Three quarters of Americans (76%) support net neutrality, including  the vast majority of Democrats and Republicans.',
+  atsi: 'Without net neutrality, {domain} could run slower in the future and be harder to access.'
 }
 const SILENCE_LENGTHS_HOURS = {
   r1: Infinity,
-  r2: 7*24,
+  r2: 5*24,
   r4: 3*24,
   r14: 2*24
 }
